35f920acbfb7040dfa489c7b721cf988bfdb823a
frappe <frappe@ubuntu.vm>
2020-10-31 09:58:02 +0000

    a

--- deretius/deretius/doctype/registro/registro.js
+++ deretius/deretius/doctype/registro/registro.js
@@ -1,12 +1,14 @@
 // Copyright (c) 2020, Pedro Antonio Fernandez Gomez and contributors
 // For license information, please see license.txt
 
+cur_frm.add_fetch("titulo", "tipo", "tipo_de_titulo");
+
 frappe.ui.form.on('Registro', {
 	onload: function(frm) {
 		frm.set_query("titulo", function() {
 			return {
 				"filters": {
-					"tipo_de_titulo": frm.doc.tipo_de_titulo
+					"tipo": frm.doc.tipo_de_titulo
 				}
 			}
 		})
