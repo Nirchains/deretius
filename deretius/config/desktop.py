8b25ebc36dc73627fd3f734bd81d275c49d1e17c
nirchains <nirchains@gmail.com>
2020-11-02 13:39:04 +0000

    config desktop

--- deretius/config/desktop.py
+++ deretius/config/desktop.py
@@ -10,5 +10,38 @@ def get_data():
 			"icon": "octicon octicon-file-directory",
 			"type": "module",
 			"label": _("Deretius")
+		},
+		{
+			"label": _("DERETIUS"),
+			"icon": "fa fa-exclamation",
+			"items": [
+				{
+					"type": "doctype",
+					"name": "Registro",
+					"description": _("Registros"),
+					"onboard": 1,
+				}
+			]
+		},
+		{
+			"label": _("Configuración"),
+			"icon": "fa fa-exclamation",
+			"items": [
+				{
+					"type": "doctype",
+					"name": "Tipo de titulo",
+					"description": _("Tipo de titulo"),
+					"onboard": 1,
+				},
+				{
+					"type": "doctype",
+					"name": "Titulo",
+					"description": _("Titulo"),
+					"onboard": 1,
+				}
+			]
 		}
 	]
+	
+		
+	
